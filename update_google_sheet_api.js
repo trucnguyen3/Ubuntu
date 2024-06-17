31589390327f9e34807ccecbc40979dd018e6cf6
Tr14 <tr14xt@gmail.com>
2024-06-18 00:47:57 +0700

    First

--- update_google_sheet_api.js
+++ update_google_sheet_api.js
@@ -26,21 +26,6 @@ const spreadsheetId = "1RA54GZKbs-ZILxXOojnSd-afygG-rov7N3O7E99Ys7k"; // Please 
 const sheetName = "vouncher"; // Please set your sheet name.
 
 app.post('/updatesheet', async (req, res) => {
-  res.json({ requestBody: "OK" })
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://dev.akadigital.net');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
   console.log(JSON.stringify(req.body))
 
   const inputValues = ["2", "used"]; // This is a sample input value.
