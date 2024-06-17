dc9dbc4c21e8ecda70889d17cc38371958cbe4fa
Tr14 <tr14xt@gmail.com>
2024-06-18 00:56:26 +0700

    First

--- update_google_sheet_api.js
+++ update_google_sheet_api.js
@@ -4,7 +4,11 @@ const express = require('express')
 const app = express()
 const port = 1337
 
+var cors = require('cors')
+
 app.use(express.json())
+
+app.use(cors())
 
 const auth = new google.auth.GoogleAuth({
   credentials: {
