f6ccf9a7d57152fd0961f7b4bf8794ecf87aec6f
Tr14 <xuantrucx111@gmail.com>
2024-06-19 15:42:59 +0700

    Third

--- update_google_sheet_api.js
+++ update_google_sheet_api.js
@@ -38,8 +38,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', '/index.html'));
 });
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', '/login.html'));
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', '/signup.html'));
 });
 
 app.get('/spinnerwheel', (req, res) => {
