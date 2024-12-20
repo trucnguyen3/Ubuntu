5a25c6eb82f6dce9378b5f14be8e8208f91c4ac4
Tr14 <xuantrucx111@gmail.com>
2024-12-20 17:32:16 +0700

    20/12/24 commit

--- update_google_sheet_api.js
+++ update_google_sheet_api.js
@@ -58,7 +58,7 @@ const appsflyer_feed = "appsflyer_feed";
 app.post('/webhook_v3', async (req, res) => {
   let data = req.body;
 
-  console.log(data)
+  console.log(data.events[0])
 
   var device_id = ""
   var platform = ""
